d5abf97091b6dcda549dca758cc2f3f76ad84db3
ainusers <5730473@qq.com>
2021-03-16 21:51:25 +0800

    SVM(线性不可分).md end

--- 2. 监督学习分类/3. 支持向量机(SVM)/线性可区分/代码/SVM-base.py
+++ 2. 监督学习分类/3. 支持向量机(SVM)/线性可区分/代码/SVM-base.py
@@ -6,11 +6,11 @@ y = [0, 0, 1]
 clf = svm.SVC(kernel = 'linear')
 clf.fit(x, y)
 
-print clf
+print (clf)
 
 # get support vectors
-print clf.support_vectors_
+print (clf.support_vectors_)
 # get indices of support vectors
-print clf.support_
+print (clf.support_)
 # get number of support vectors for each class
-print clf.n_support_
+print (clf.n_support_)
