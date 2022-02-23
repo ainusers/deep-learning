7e845c02ca16a4226e2636c2ca518edd5321bee4
ainuser <5730473@qq.com>
2022-02-23 18:20:12 +0800

    Update KNN-custom.py
    feat: 修改KNN代码

--- 入门篇/基础/2. 监督学习 - 分类/2. 最邻近规则分类(KNN)/代码/KNN-custom.py
+++ 入门篇/基础/2. 监督学习 - 分类/2. 最邻近规则分类(KNN)/代码/KNN-custom.py
@@ -2,11 +2,19 @@ import csv
 import random
 import math
 import operator
-
-
+ 
+ 
 def loadDataset(filename, split, trainingSet = [], testSet = []):
-    with open(filename, 'rb') as csvfile:
-        lines = csv.reader(csvfile)
+    '''
+    导入数据
+    :param filename:
+    :param split: 将数据总集以split为界限 分成训练集和测试集
+    :param trainingSet:
+    :param testSet:
+    :return:
+    '''
+    with open(filename, 'rt') as csvfile:       # 以逗号为分隔符
+        lines = csv.reader(csvfile)             # 读取所有行
         dataset = list(lines)
         for x in range(len(dataset)-1):
             for y in range(4):
@@ -15,16 +23,30 @@ def loadDataset(filename, split, trainingSet = [], testSet = []):
                 trainingSet.append(dataset[x])
             else:
                 testSet.append(dataset[x])
-
-
+ 
+ 
 def euclideanDistance(instance1, instance2, length):
+    '''
+    计算euclideanDistance
+    :param instance1:
+    :param instance2:
+    :param length: 维度
+    :return:
+    '''
     distance = 0
     for x in range(length):
         distance += pow((instance1[x]-instance2[x]), 2)
     return math.sqrt(distance)
-
-
+ 
+ 
 def getNeighbors(trainingSet, testInstance, k):
+    '''
+    返回最近的k个邻居
+    :param trainingSet: 训练集
+    :param testInstance: 一个测试实例
+    :param k: 参数k
+    :return:
+    '''
     distances = []
     length = len(testInstance)-1
     for x in range(len(trainingSet)):
@@ -37,9 +59,14 @@ def getNeighbors(trainingSet, testInstance, k):
     for x in range(k):
         neighbors.append(distances[x][0])
         return neighbors
-
-
+ 
+ 
 def getResponse(neighbors):
+    '''
+    以距离排序，返回最近的几个点
+    :param neighbors:
+    :return:
+    '''
     classVotes = {}
     for x in range(len(neighbors)):
         response = neighbors[x][-1]
@@ -47,26 +74,32 @@ def getResponse(neighbors):
             classVotes[response] += 1
         else:
             classVotes[response] = 1
-    sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)  # python3 里的.items()返回的是列表，.iteritems()返回的是一个迭代器
     return sortedVotes[0][0]
-
-
+ 
+ 
 def getAccuracy(testSet, predictions):
+    '''
+    预测值和实际值的准确率
+    :param testSet:
+    :param predictions:
+    :return:
+    '''
     correct = 0
     for x in range(len(testSet)):
         if testSet[x][-1] == predictions[x]:
             correct += 1
     return (correct/float(len(testSet)))*100.0
-
-
+ 
+ 
 def main():
     #prepare data
     trainingSet = []
     testSet = []
     split = 0.67
-    loadDataset('data.txt', split, trainingSet, testSet)
-    print ('Train set: ' + repr(len(trainingSet)))
-    print ('Test set: ' + repr(len(testSet)))
+    loadDataset(r'data.txt', split, trainingSet, testSet)
+    print('Train set: ' + repr(len(trainingSet)))
+    print('Test set: ' + repr(len(testSet)))
     #generate predictions
     predictions = []
     k = 3
@@ -76,39 +109,9 @@ def main():
         result = getResponse(neighbors)
         predictions.append(result)
         print ('>predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
-        accuracy = getAccuracy(testSet, predictions)
-        print('Accuracy: ' + repr(accuracy) + '%')
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy)+ '%')
+ 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
